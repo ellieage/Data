b736c03753db4c513eabd61c95ee7f053d310148
Erik Perkins <erik.perkins@appfolio.com>
2016-10-01 13:15:10 -0700

    ep - Amend Spark scripts to allow S3 access

--- s3/spark/spark-app.sh
+++ s3/spark/spark-app.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+export AWS_ACCESS_KEY_ID=my_aws_acces_key_id
+export AWS_SECRET_ACCESS_KEY=my_aws_secret_access_key
+
+# export SPARK_CLASSPATH='/path/xxx.jar:/path/xx2.jar'
+# spark-defaults.conf : spark.driver.extraClassPath /path/to/my.jar
+# spark-defaults.conf : spark.executor.extraClassPath /path/to/my.jar 
+
 spark-submit \
-  --class "Beowulf" \
-  --master local \
   --jars jars/aws-java-sdk-1.7.4.jar,jars/hadoop-aws-2.7.2.jar \
+  --master local \
+  --class "Beowulf" \
   target/scala-2.11/simple-spark-project_2.11-0.1.0.jar
--- s3/spark/spark-notebook.sh
+++ s3/spark/spark-notebook.sh
@@ -1,6 +1,11 @@
+#!/usr/bin/env bash
+
 export PYSPARK_DRIVER_PYTHON=ipython
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777"
+export AWS_ACCESS_KEY_ID=my_aws_access_key_id
+export AWS_SECRET_ACCESS_KEY=my_aws_secret_access_key
 
 pyspark \
   --packages com.databricks:spark-csv_2.10:1.1.0 \
+  --jars jars/aws-java-sdk-1.7.4.jar,jars/hadoop-aws-2.7.2.jar \
   --master local
