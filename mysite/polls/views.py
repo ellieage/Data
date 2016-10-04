9a3d76ca229c3cae252939bc8a636b2d4d58a889
Erik Perkins <erik.perkins@appfolio.com>
2016-10-04 13:51:06 -0700

    ep - Change preposition

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -27,7 +27,7 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're at question %s." % question_id)
+    return HttpResponse("You're on question %s." % question_id)
 
 def results(request, question_id):
     response = "You're at the results of question %s."
