fc21b69636a1926b122c89302a31360bc8e75ec8
Ellie Essen <ellie.essen@gmail.com>
2016-10-10 13:30:08 -0700

    End of Django app Part 3:
    writing views

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':'django.db.backends.postgresql',
+        'NAME':'django',
+        'USER':'django',
+        'PASSWORD':'django',
+        'HOST':'127.0.01',
+        'PORT':'5432'
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
